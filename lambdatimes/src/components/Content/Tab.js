ded2af3789ecc8ea959b1cc3f3bd68f85f885375
BrandonSaravia <BPSaravia@gmail.com>
2019-03-29 13:58:14 -0400

    completed all mvp except for adding functionality for filtering my cards.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Tab = props => {
+  if(props.tab === props.selectedTab){
+    return {className: 'tab active-tab'}
+  }
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
       className={''}
-      onClick={() => {
+      onClick={(newTab) => {
+        props.selectTabHandler(newTab)
+        
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
@@ -18,5 +24,11 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  selectTabHandler: PropTypes.func.isRequired,
+  tab: PropTypes.srting,
+  selectedTab: PropTypes.func.isRequired
+};
+
 
 export default Tab;
